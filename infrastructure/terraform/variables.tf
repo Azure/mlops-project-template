6a38001751b6122f7477786f44972b18e8585ddd
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-11-14 14:22:00 +0000

    rollback sec ws merge

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -20,23 +20,25 @@ variable "postfix" {
 
 variable "enable_aml_computecluster" {
   description = "Variable to enable or disable AML compute cluster"
+  default = true
 }
 
 variable "enable_aml_secure_workspace" {
   description = "Variable to enable or disable AML secure workspace"
+  default = false
 }
 
-variable "jumphost_username" {
-  type        = string
-  description = "VM username"
-  default     = "azureuser"
-}
+# variable "jumphost_username" {
+#   type        = string
+#   description = "VM username"
+#   default     = "azureuser"
+# }
 
-variable "jumphost_password" {
-  type        = string
-  description = "VM password"
-  default     = "ThisIsNotVerySecure!"
-}
+# variable "jumphost_password" {
+#   type        = string
+#   description = "VM password"
+#   default     = "ThisIsNotVerySecure!"
+# }
 
 variable "enable_monitoring" {
   description = "Variable to enable or disable Monitoring"
