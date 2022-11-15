ec93bcf8d05fe7afb215e046a12ae394aa42ce98
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-11-15 21:39:14 +0000

    rollback tf sec ws

--- infrastructure/terraform/modules/data-explorer/outputs.tf
+++ infrastructure/terraform/modules/data-explorer/outputs.tf
@@ -1,11 +1,1 @@
-output "id" {
-  value = azurerm_kusto_cluster.cluster[0].id
-}
 
-output "uri" {
-  value = azurerm_kusto_cluster.cluster[0].uri
-}
-
-output "name" {
-  value = azurerm_kusto_database.database[0].name
-}
