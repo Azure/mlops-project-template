efab6c76b5aab27648c640c81096a948d3dc5060
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-11-30 16:38:22 +0000

    fixed syntax issue with monitoring

--- classical/rai-aml-cli-v2/data-science/src/prep/prep.py
+++ classical/rai-aml-cli-v2/data-science/src/prep/prep.py
@@ -101,8 +101,8 @@ def main(args):
     val.to_parquet((Path(args.val_data) / "val.parquet"))
     test.to_parquet((Path(args.test_data) / "test.parquet"))
 
-    if (args.enable_monitoring.lower == 'true' or args.enable_monitoring == '1' or args.enable_monitoring.lower == 'yes'):
-        log_training_data(data, args.table_name)
+    if (args.enable_monitoring.lower() == 'true' or args.enable_monitoring == '1' or args.enable_monitoring.lower() == 'yes'):
+        log_training_data(df, args.table_name)
 
 
 if __name__ == "__main__":
